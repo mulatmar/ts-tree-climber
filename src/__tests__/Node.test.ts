81b8555163b251048c48fe70dfa404af4d53330d
Mulatmar <mulatmar@mail.ru>
2021-01-25 23:51:14 +0300

    rename test

--- src/__tests__/Node.test.ts
+++ src/__tests__/Node.test.ts
@@ -1,5 +1,5 @@
 import { Tree } from '../index';
-test('My Greeter', () => {
+test('My Tree', () => {
     const tree = new Tree(null);
     const root = tree.parse({id: 1});
     expect(root.model.id).toBe(1);
